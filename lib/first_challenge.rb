06c9c6578dee2be9e3f929fcbb5e6ce65754d5df
Meg Galvez <meg@oakmaine.com>
2019-04-02 15:10:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -25,6 +25,7 @@ contacts.each do |person, data|
         # here, each index element in an ice cream flavor string
         flavor.delete_if == "strawberry"
       end 
+    end 
   end
 end
   #remember to return your newly altered contacts hash!
