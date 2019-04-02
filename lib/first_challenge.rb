f4e06b33f1507a8fd7d3553223c6ceb1d81402f3
Meg Galvez <meg@oakmaine.com>
2019-04-02 14:59:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,6 +15,7 @@ def first_challenge
 
   #your code here
 contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
+contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
 
   #remember to return your newly altered contacts hash!
   contacts
